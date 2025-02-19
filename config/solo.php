41d0759c10ce91d7915d33494d616606694f7b1a
Ryan Haudenschilt <rhaudenschilt@contactusinc.com>
2025-02-19 14:29:50 -0500

    Added footer with github link. Upgraded to Laravel 11. Added laravel solo support.

---
+++ config/solo.php
@@ -0,0 +1,66 @@
+<?php
+
+use SoloTerm\Solo\Commands\Command;
+use SoloTerm\Solo\Commands\EnhancedTailCommand;
+use SoloTerm\Solo\Commands\MakeCommand;
+use SoloTerm\Solo\Hotkeys as Hotkeys;
+use SoloTerm\Solo\Themes as Themes;
+
+// Solo may not (should not!) exist in prod, so we have to
+// check here first to see if it's installed.
+if (!class_exists('\SoloTerm\Solo\Manager')) {
+    return [
+        //
+    ];
+}
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Themes
+    |--------------------------------------------------------------------------
+    */
+    'theme' => env('SOLO_THEME', 'dark'),
+
+    'themes' => [
+        'light' => Themes\LightTheme::class,
+        'dark' => Themes\DarkTheme::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Keybindings
+    |--------------------------------------------------------------------------
+    */
+    'keybinding' => env('SOLO_KEYBINDING', 'default'),
+
+    'keybindings' => [
+        'default' => Hotkeys\DefaultHotkeys::class,
+        'vim' => Hotkeys\VimHotkeys::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Commands
+    |--------------------------------------------------------------------------
+    |
+    */
+    'commands' => [
+        'HTTP' => 'php artisan serve',
+        'Logs' => EnhancedTailCommand::file(storage_path('logs/laravel.log')),
+        'NPM'  => 'npm run watch',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Miscellaneous
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+     * If you run the solo:dumps command, Solo will start a server to receive
+     * the dumps. This is the address. You probably don't need to change
+     * this unless the default is already taken for some reason.
+     */
+    'dump_server_host' => env('SOLO_DUMP_SERVER_HOST', 'tcp://127.0.0.1:9984')
+];
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -61,4 +61,15 @@ function optgroupMatcher (params, data) {
   return null;
 }
 </script>
+
+<div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+            <span class="mb-3 mb-md-0 text-body-secondary">Soccer Team Manager</span>
+        </div>
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="link-secondary" href="https://github.com/ryanhowdy/soccer-team-manager"><i class="bi bi-github"></i></a></li>
+        </ul>
+    </footer>
+</div>
 </html>
