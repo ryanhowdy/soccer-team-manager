7098a00c7b2c3421c9e209c8d0282a52a885d427
Ryan Haudenschilt <rhaudenschilt@contactusinc.com>
2025-04-29 10:53:29 -0400

    Added dates to all the small game listings.

--- resources/views/games/game-h2h.blade.php
+++ resources/views/games/game-h2h.blade.php
@@ -48,26 +48,29 @@
             <div class="game-listing-small rounded rounded-3 bg-white p-4 mb-3">
             @foreach($results as $result)
                 <a href="{{ route('games.show', ['id' => $result->id]) }}" 
-                    class="home-v-away d-grid align-items-center justify-content-center mb-3 text-decoration-none rounded rounded-2 text-dark">
-                    <div class="home-team d-flex align-items-center justify-content-end">
-                        <div class="me-2">{{ $result->homeTeam->name }}</div>
-                        <img class="logo img-fluid" data-bs-toggle="tooltip" data-bs-title="{{ $result->homeTeam->club->name }}" 
-                            src="{{ asset($result->homeTeam->club->logo) }}" onerror="this.onerror=null;this.src='{{ asset('img/logo_none.png') }}';"/>
-                    </div>
-                    <div class="score text-center">
-                        <span @class([
-                            'badge',
-                            'rounded-pill',
-                            'text-white',
-                            'bg-success'   => ($result->win_draw_loss == 'W'),
-                            'bg-secondary' => ($result->win_draw_loss == 'D'),
-                            'bg-danger' => ($result->win_draw_loss == 'L'),
-                        ])>{{ $result->home_team_score }} - {{ $result->away_team_score }}</span>
-                    </div>
-                    <div class="away-team d-flex align-items-center">
-                        <img class="logo img-fluid" data-bs-toggle="tooltip" data-bs-title="{{ $result->awayTeam->club->name }}"
-                            src="{{ asset($result->awayTeam->club->logo) }}" onerror="this.onerror=null;this.src='{{ asset('img/logo_none.png') }}';"/>
-                        <div class="ms-2">{{ $result->awayTeam->name }}</div>
+                    class="mb-3 text-decoration-none rounded rounded-2 text-dark">
+                    <div class="small text-center text-secondary">{{ $result->date->inUserTimezone()->format('M j, Y') }}</div>
+                    <div class="home-v-away d-grid align-items-center justify-content-center mb-3">
+                        <div class="home-team d-flex align-items-center justify-content-end">
+                            <div class="me-2">{{ $result->homeTeam->name }}</div>
+                            <img class="logo img-fluid" data-bs-toggle="tooltip" data-bs-title="{{ $result->homeTeam->club->name }}" 
+                                src="{{ asset($result->homeTeam->club->logo) }}" onerror="this.onerror=null;this.src='{{ asset('img/logo_none.png') }}';"/>
+                        </div>
+                        <div class="score text-center">
+                            <span @class([
+                                'badge',
+                                'rounded-pill',
+                                'text-white',
+                                'bg-success'   => ($result->win_draw_loss == 'W'),
+                                'bg-secondary' => ($result->win_draw_loss == 'D'),
+                                'bg-danger' => ($result->win_draw_loss == 'L'),
+                            ])>{{ $result->home_team_score }} - {{ $result->away_team_score }}</span>
+                        </div>
+                        <div class="away-team d-flex align-items-center">
+                            <img class="logo img-fluid" data-bs-toggle="tooltip" data-bs-title="{{ $result->awayTeam->club->name }}"
+                                src="{{ asset($result->awayTeam->club->logo) }}" onerror="this.onerror=null;this.src='{{ asset('img/logo_none.png') }}';"/>
+                            <div class="ms-2">{{ $result->awayTeam->name }}</div>
+                        </div>
                     </div>
                 </a>
             @endforeach
